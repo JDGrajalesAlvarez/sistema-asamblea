c8e8b2b7c1e040885d1ecf2c612176170f465331
JDGrajalesAlvarez <juandagrajalesgrajales@gmail.com>
2026-02-17 15:27:06 -0500

    fix de pantalla carga x2

--- src/pages/PantallaCarga.jsx
+++ src/pages/PantallaCarga.jsx
@@ -50,7 +50,7 @@ function PantallaCarga() {
                 </h2>
 
                 <div className="carga-porcentaje">
-                    {totalCoeficiente.toFixed(3)}%
+                    {totalCoeficiente.toFixed(2)}%
                 </div>
 
                 <div className="progress-bar-container">
